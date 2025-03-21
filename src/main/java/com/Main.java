cdf32f4e6a098ad115abb4e0d6a4b65b1b29e341
mili1312 <118750039+mili1312@users.noreply.github.com>
2025-03-21 06:16:42 +0200

    FinalTikta

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -4,8 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main extends Application {
@@ -16,10 +21,29 @@ public class Main extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainView.fxml"));
         Parent root = loader.load();
 
+        // Get references to the button and image view
+        Button uploadButton = (Button) loader.getNamespace().get("uploadButton");
+        ImageView imageView = (ImageView) loader.getNamespace().get("imageView");
+
         // Set the scene
         primaryStage.setTitle("Travel Journal");
         primaryStage.setScene(new Scene(root, 600, 400)); // Set size to match your FXML
         primaryStage.show();
+
+        // Add action to the upload button
+        uploadButton.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif", "*.bmp"));
+
+            // Show open file dialog
+            File file = fileChooser.showOpenDialog(primaryStage);
+
+            if (file != null) {
+                // Load the image and display it in the ImageView
+                Image image = new Image(file.toURI().toString());
+                imageView.setImage(image);
+            }
+        });
     }
 
     public static void main(String[] args) {
