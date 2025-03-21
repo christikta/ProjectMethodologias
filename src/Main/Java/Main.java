a9787535296efa493e4cb14a3ea9c813e4768339
mili1312 <miliadisili@yahoo.gr>
2025-03-21 05:29:48 +0200

    Initial commit

---
+++ src/Main/Java/Main.java
@@ -0,0 +1,28 @@
+package Main.Java;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Load the FXML file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainView.fxml"));
+        Parent root = loader.load();
+
+        // Set the scene
+        primaryStage.setTitle("JavaFX App");
+        primaryStage.setScene(new Scene(root, 600, 400)); // Set size to match your FXML
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
